f947dfc9dc55ce42bf7cdba36ceb6410941d955d
unaisali96 <84286247+unaisali96@users.noreply.github.com>
2021-05-16 22:55:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//This command use to print on
+//Google
+console.log("Welcome to js")
+//Variable
+
+//var name = 'Unais';
+//console.log(name);
+
+//var age = 23;
+//console.log(age)
+
+var firstname = 'Unais';
+var lastname = 'ali';
+var space= ' '; //for creating space
+
+ console.log(firstname + space + lastname);
+ console.log(firstname +' '+ lastname +space+ 'My Age is ='+ age); // for beginners
+ var age = prompt("What is your age?");
+ document.getElementById('myage').innerHTML=age;
+
